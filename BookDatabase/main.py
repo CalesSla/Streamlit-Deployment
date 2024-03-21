ff16fae308e22e6840524d62527024ee4f75b212
Slava <veaceslavcalestru@gmail.com>
2024-03-21 14:47:21 +0200

    API call project

---
+++ BookDatabase/main.py
@@ -0,0 +1,56 @@
+import streamlit as st
+import pandas as pd
+import requests
+
+st.title(":books: Search Project Gutenberg")
+
+@st.cache_data
+def search_gutenberg(author, title):
+    BASE_URL = "https://gutendex.com/books?search="
+    author = author.replace(" ", "%20")
+    title = title.replace(" ", "%20")
+    params_url = f"{author}%20{title}"
+    search_url = f"{BASE_URL}{params_url}"
+    try:
+        res = requests.get(search_url)
+        json_res = res.json()
+        if json_res['count'] == 0:
+            return False
+        else:
+            return json_res
+    except:
+        return False
+    
+@st.cache_data
+def format_json_res(json_res):
+    cols = ["Id", "Author", "Title", "Language", "Link"]
+    rows = []
+    try:
+        for result in json_res['results']:
+            id = result['id']
+            author = result['authors'][0]['name']
+            title = result['title']
+            language = result['languages'][0]
+            link = f"https://www.gutenberg.org/ebooks/{id}"
+            rows.append([id, author, title, language, link])
+
+        df = pd.DataFrame(rows, columns = cols)
+        return df
+    except:
+        st
+
+with st.form("Main form"):
+    col1, col2 = st.columns(2)
+    author = col1.text_input("Author")
+    title = col2.text_input("Title")
+
+    search = st.form_submit_button("Search", type='primary')
+
+    if search:
+        json_res = search_gutenberg(author, title)
+        if json_res:
+            df = format_json_res(json_res)
+            st.subheader("Results")
+            st.table(df)
+        else:
+            st.error("No results found")
